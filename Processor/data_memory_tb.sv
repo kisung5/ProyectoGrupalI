238014d4ce046ba0493738ba67a0a7ab2673aa35
Ki Sung Lim <kisungqueso@hotmail.com>
2020-06-26 16:14:47 -0600

    New data with new memory. (REV)
    Needs revision.

--- Processor/data_memory_tb.sv
+++ Processor/data_memory_tb.sv
@@ -7,18 +7,27 @@ logic [7:0] data, q;
 
 // address, clock, data, wren, q
 
-data_memory DUT (.address(addr), .clock(clk), .wren(wren), .data(data), .q(q));
+data_memory DUT (.address(addr), .clock(~clk), .wren(wren), .data(data), .q(q));
 
-always #1 clk <= ~clk;
+always #10 clk <= ~clk;
 
 initial begin
 	clk = 0;
 	wren = 0;
 	data = 8'b0;
-	addr = 20'h0;
-	#100;
+	#5 addr = 20'h0;
+	#50;
 	#5 addr = 20'h1;
-	#100;
+	#50;
+	#5 addr = 20'h2;
+	#50;
+	#5 addr = 20'h3;
+	#50;
+	#5 addr = 20'h4;
+	#50;
+	#5 addr = 20'h95fff;
+	#50;
+	#5 addr = 20'h96000;
 end
 
 endmodule 
